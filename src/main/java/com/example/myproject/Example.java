ea0b576e4f20ef596e573552ead850db5f47a25e
kortyeltefari <kortyel@tefari.com>
2019-06-23 15:49:42 -0500

    web driver

--- src/main/java/com/example/myproject/Example.java
+++ src/main/java/com/example/myproject/Example.java
@@ -37,40 +37,42 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 @EnableAutoConfiguration
 public class Example {
 
-	@CrossOrigin(origins = "http://spring.local:8080/")
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
-	Map home(@RequestParam(value="img", required=false) String img) {
+	Map home(@RequestParam(value = "img", required = false) String img) {
 
 		Map result = null;
 		// WebDriver ->get google image --> "avacado
-
-		String imgUrl = "https://firebasestorage.googleapis.com/v0/b/trueorgs.appspot.com/o/test.jpg?alt=media";
+		String params = "f756b834-0ad5-477e-bdc3-25d5d8e9c6bf";
+		params = img;
+		String imgUrl = "https://firebasestorage.googleapis.com/v0/b/trueorgs.appspot.com/o/test.jpg?alt=media&token="
+				
+				+ params;
 		String gurl = "https://images.google.com/";
+		System.out.println("IMAGE URL: "+imgUrl);
 		String purl = "http://192.168.33.10/php/form.html";
 		// processRequest(purl,imgUrl);
-		
-		//?sourceImg={https://firebasestorage.googleapis.com/v0/b/trueorgs.appspot.com/o/test.jpg?alt=media&token=0ea99427-a248-4ff2-acd9-a2ed6984deec}
-		
+
+		// ?sourceImg={https://firebasestorage.googleapis.com/v0/b/trueorgs.appspot.com/o/test.jpg?alt=media&token=0ea99427-a248-4ff2-acd9-a2ed6984deec}
+
 		result = Collections.singletonMap("response", fetchWebDriverResponseString(imgUrl));
 
 		return result;
 	}
 
 	public static String fetchWebDriverResponseString(String sourceImage) {
-		 FirefoxBinary firefoxBinary = new FirefoxBinary();
-		    firefoxBinary.addCommandLineOptions("--headless");		    
-		    FirefoxOptions firefoxOptions = new FirefoxOptions();
-		    firefoxOptions.setBinary(firefoxBinary);
-		    FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
-		String sourceImgUrl = "https://firebasestorage.googleapis.com/v0/b/trueorgs.appspot.com/o/test.jpg?alt=media";
-		sourceImgUrl = sourceImage;
+		FirefoxBinary firefoxBinary = new FirefoxBinary();
+		firefoxBinary.addCommandLineOptions("--headless");
+		FirefoxOptions firefoxOptions = new FirefoxOptions();
+		firefoxOptions.setBinary(firefoxBinary);
+		FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
 		System.out.println("Web Driver started");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.navigate().to("https://images.google.com/");
 		WebElement element = driver.findElement(By.className("S3Wjs"));
 		element.click();
 		element = driver.findElement(By.id("qbui"));
-		element.sendKeys(sourceImgUrl);
+		element.sendKeys(sourceImage);
 		element.submit();
 		element = driver.findElement(By.className("fKDtNb"));
 		String resp = null;
